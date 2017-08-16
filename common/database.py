82517232aefeba6f083a62cf2a190c421bc8697f
SNSN2033 <sy2627@columbia.edu>
2017-08-16 13:37:47 -0700

    Add files via upload

---
+++ common/database.py
@@ -0,0 +1,30 @@
+import pymongo
+
+
+class Database(object):
+    URI = ['localhost:27017']
+    DATABASE = None
+
+    @staticmethod
+    def initialize():
+        client = pymongo.MongoClient(Database.URI)
+        Database.DATABASE = client['project']
+
+    #create a form named collection with data in the current database 'project'
+    @staticmethod
+    def insert(collection, data):
+        Database.DATABASE[collection].insert(data)
+
+    @staticmethod
+    def find(collection, query):
+        return Database.DATABASE[collection].find(query)
+
+    @staticmethod
+    def find_one(collection, query):
+        return Database.DATABASE[collection].find_one(query)
+
+    @staticmethod
+    def remove(collection, query):
+        Database.DATABASE[collection].remove(query)
+
+
